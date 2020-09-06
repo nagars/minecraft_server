4354f8558696b595c7880c51a83bf6787c054e31
shawn <shawnnagar@gmail.com>
2020-09-06 19:40:11 +0530

    Fixed bug with update script failing in the startup script. It could not download the version manifest from the minecraft website because the network driver was still not intialised (no access to internet). In the startup script I added  code to wait till the network is running before calling the update script.

--- install_guide/backup.sh
+++ install_guide/backup.sh
@@ -1,31 +1,41 @@
-#/bin/sh
+#!/bin/sh
 
+#Path to server folder directory
 server_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" ; pwd -P)
 
-echo ${server_path}
+#echo ${server_path}
 
+#Name of server folder
 folder_name="${server_path##*/}"
 
-echo ${folder_name}
+#echo ${folder_name}
 
-parent_path=$(pwd -P)
+#Extracts parent path (Before main server folder)
+parent_path=$(cd ".." ; pwd -P) 
 
-echo ${parent_path}
+#echo ${parent_path}
 
+#Checks if the backup folder has been made. If not, it makes it.
 if [ ! -d server_backup ]
 then
 	mkdir server_backup
 fi
 
+#Generates the server backup path
 backup_path=$(cd "${parent_path}/server_backup"; pwd -P);
 
-echo ${backup_path}
+#echo ${backup_path}
 
+#Check if the server folder for backups exists. If not, it makes it.
 if [ ! -d ${folder_name} ]
 then
 	mkdir ${folder_name}
 fi
 
-echo ${backup_path}/${folder_name}
+#echo ${backup_path}/${folder_name}
 
+# Delete older backup
+find "${backup_path}/${folder_name}" -type f -name '*.gz' -delete
+
+# Create new backup
 tar -cvpzf "${backup_path}/${folder_name}"-$(date +%F-%H-%M).tar.gz ${server_path}
